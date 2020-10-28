b3d299e1cec6d8502a5a00f5efe37d1ec12f63d8
AnastasiaErm <anastasia_ermolova@bk.ru>
2020-10-28 21:37:27 +0300

    Updates

---
+++ precache-manifest.705f68d4a5388a658fc59ef6d2ca49c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467aa4bb5dae42d922f33cc2e9a1dcbf",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "37e8cc940bdb76ee80dc",
+    "url": "/spacex/static/css/main.fa0b50a0.chunk.css"
+  },
+  {
+    "revision": "f393db74116284b6757d",
+    "url": "/spacex/static/js/2.3a6e78e4.chunk.js"
+  },
+  {
+    "revision": "88c0bfd11a2be5419414edbddb842b0f",
+    "url": "/spacex/static/js/2.3a6e78e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e8cc940bdb76ee80dc",
+    "url": "/spacex/static/js/main.b86cb7be.chunk.js"
+  },
+  {
+    "revision": "38bd822f86f0fa518f7f",
+    "url": "/spacex/static/js/runtime-main.3f7de82f.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spacex/static/media/logo.d5ab31ce.svg"
+  }
+]);
